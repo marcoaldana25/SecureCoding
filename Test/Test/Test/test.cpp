5da8948f6e6c73d568f1b77a00e2e1f1993bd5ef
marcoaldana25 <75954461+marcoaldana25@users.noreply.github.com>
2021-07-24 17:08:22 -0500

    Completed 4 of 13 unit tests for Module Four Milestone

--- Test/Test/Test/test.cpp
+++ Test/Test/Test/test.cpp
@@ -90,24 +90,81 @@ TEST_F(CollectionTest, CanAddToEmptyVector)
 {
     // is the collection empty?
     // if empty, the size must be 0
-
+    ASSERT_EQ(collection->size(), 0);
+	
     add_entries(1);
-
     // is the collection still empty?
     // if not empty, what must the size be?
+    ASSERT_EQ(collection->size(), 1);
 }
 
 // TODO: Create a test to verify adding five values to collection
 TEST_F(CollectionTest, CanAddFiveValuesToVector)
 {
     add_entries(5);
+
+	// Assert that 5 elements existing within the collection vector.
+    ASSERT_EQ(collection->size(), 5);
 }
 
 // TODO: Create a test to verify that max size is greater than or equal to size for 0, 1, 5, 10 entries
+TEST_F(CollectionTest, VerifyMaxSizeIsGreaterThanOrEqualToEntrySize)
+{
+	// Collection will be empty on SetUp so no need to attempt to add 0.
+	// Plus, Assert(count > 0) will prevent from moving further.
+    ASSERT_GE(collection->max_size(), 0);
+
+    // Add a single entry, then tear down before the next test.
+    add_entries(1);
+    ASSERT_GE(collection->max_size(), 1);
+    TearDown();
+
+    // Before next entry, setup collection to prevent null reference
+    SetUp();
+
+    // Add a five entries, then tear down before the next test.
+    add_entries(5);
+    ASSERT_GE(collection->max_size(), 5);
+    TearDown();
+
+    // Add ten entries, do not tear down because that will be handled as the test ends.
+    SetUp();
+    add_entries(10);
+    ASSERT_GE(collection->max_size(), 10);
+}
 
 // TODO: Create a test to verify that capacity is greater than or equal to size for 0, 1, 5, 10 entries
+TEST_F(CollectionTest, VerifyCapacityIsGreaterThanOrEqualToEntrySize)
+{
+	// Assert that collection's capacity is greater than or equal to 0 immediately.
+    ASSERT_GE(collection->capacity(), 0);
+
+	// Add a single entry, assert capacity is greater than or equal to 1, finally tear down before next test.
+    add_entries(1);
+    ASSERT_GE(collection->capacity(), 1);
+    TearDown();
+
+	// Before next entry, setup collection to prevent null reference.
+    SetUp();
+
+	// Add five entries, assert capacity is greater than or equal to 5, finally tear down before next test.
+    add_entries(5);
+    ASSERT_GE(collection->capacity(), 5);
+    TearDown();
+
+	// Before next entry, setup collection to prevent null reference.
+    SetUp();
+
+	// Add ten entries, assert capacity is greater than or equal to 10, and do not tear down.
+    add_entries(10);
+    ASSERT_GE(collection->capacity(), 10);
+}
 
 // TODO: Create a test to verify resizing increases the collection
+TEST_F(CollectionTest, VerifyResizeIncreasesCollection)
+{
+	
+}
 
 // TODO: Create a test to verify resizing decreases the collection
 
