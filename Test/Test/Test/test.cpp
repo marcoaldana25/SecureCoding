c4250540f0765946033bae7ffff5d3b663117731
marcoaldana25 <75954461+marcoaldana25@users.noreply.github.com>
2021-07-24 19:26:35 -0500

    Complete remainder of Unit Tests

--- Test/Test/Test/test.cpp
+++ Test/Test/Test/test.cpp
@@ -163,20 +163,149 @@ TEST_F(CollectionTest, VerifyCapacityIsGreaterThanOrEqualToEntrySize)
 // TODO: Create a test to verify resizing increases the collection
 TEST_F(CollectionTest, VerifyResizeIncreasesCollection)
 {
-	
+	// First, Assert that size is 0 due to new vector instantiation.
+    ASSERT_EQ(collection->size(), 0);
+
+    // Perform resize function to increase vector size to 5
+    collection->resize(5);
+
+	// It is expected that the size is now greater than the original size of zero.
+    EXPECT_GT(collection->size(), 0);
+
+	// Add additional assertion to verify size is now 5 per resize() call.
+    ASSERT_EQ(collection->size(), 5);
 }
 
 // TODO: Create a test to verify resizing decreases the collection
+TEST_F(CollectionTest, VerifyResizeDecreasesCollection)
+{
+    // First, Add entries to populate collection.
+    add_entries(5);
+
+	// Assert vector size is equal to five
+    ASSERT_EQ(collection->size(), 5);
+
+	// Perform vector resize to 3
+    collection->resize(3);
+
+	// It is expected that the size is now less than the original size of five
+    EXPECT_LT(collection->size(), 5);
+
+	// Add additional assert to verify size is now 3 per resize() call.
+    ASSERT_EQ(collection->size(), 3);
+}
 
 // TODO: Create a test to verify resizing decreases the collection to zero
+TEST_F(CollectionTest, VerifyResizeDecreasesCollectionToZero)
+{
+    // First, Add entries to populate collection.
+    add_entries(10);
+
+	// Assert collection has 10 elements
+    ASSERT_EQ(collection->size(), 10);
+
+	// Perform vector resize to 0
+    collection->resize(0);
+
+	// It is expected that the collection size will now be 0
+    EXPECT_EQ(collection->size(), 0);
+}
 
 // TODO: Create a test to verify clear erases the collection
+TEST_F(CollectionTest, VerifyClearErasesCollection)
+{
+    // First, Add entries to populate collection.
+    add_entries(5);
 
-// TODO: Create a test to verify erase(begin,end) erases the collection
+	// Assert collection has 5 elements
+    ASSERT_EQ(collection->size(), 5);
+
+	// Perform clear function
+    collection->clear();
+
+	// It is expected that the collection after performing clear() will now
+	// contain 0 element.
+    EXPECT_EQ(collection->size(), 0);
+}
+
+// TODO: Create a test to verify erase(begin,end) erases the collection :
+TEST_F(CollectionTest, VerifyEraseErasesCollectionUsingBeginEnd)
+{
+	// First, Add entries to populate collection.
+    add_entries(5);
+
+	// Assert collection has 5 elements
+    ASSERT_EQ(collection->size(), 5);
+
+	// Perform erase method using vector's begin and end methods as parameters
+    collection->erase(collection->begin(), collection->end());
+
+	// It is expected that the collection will now be empty and have a size of 0.
+    ASSERT_EQ(collection->size(), 0);
+
+}
 
 // TODO: Create a test to verify reserve increases the capacity but not the size of the collection
+TEST_F(CollectionTest, VerifyReserveIncreasesCapacacityNotSize)
+{
+	// First, Add Entries to populate collection
+    add_entries(2);
+
+	// Assert both size and capacity are equal to 2.
+    ASSERT_EQ(collection->size(), 2);
+    ASSERT_EQ(collection->capacity(), 2);
+
+	// Perform reserve function
+    collection->reserve(4);
+
+	// Assert collection size is equal to 2.
+    ASSERT_EQ(collection->size(), 2);
+
+	// Assert collection capacity is now equal to 4.
+    ASSERT_EQ(collection->capacity(), 4);
+}
 
 // TODO: Create a test to verify the std::out_of_range exception is thrown when calling at() with an index out of bounds
 // NOTE: This is a negative test
+TEST_F(CollectionTest, VerifyAtThrowsOutOfRangeExceptionOnIndex)
+{
+	// Verify size of collection is zero after SetUp()
+    ASSERT_EQ(collection->size(), 0);
+
+    ASSERT_THROW(collection->at(1), std::out_of_range);
+}
 
 // TODO: Create 2 unit tests of your own to test something on the collection - do 1 positive & 1 negative
+TEST_F(CollectionTest, VerifyShrinkToFitUpdatesCapactityToSize)
+{
+    // First, Add entries to populate collection
+    add_entries(5);
+
+    // Assert collection size is equal to 5
+    ASSERT_EQ(collection->size(), 5);
+
+    // Perform reserve to increase capacity of collection
+    collection->reserve(10);
+
+	// Assert capacity increased to 10
+    ASSERT_EQ(collection->capacity(), 10);
+
+	// Perform shrink_to_fit() method to re-sync capacity and size
+    collection->shrink_to_fit();
+
+    // It is expected that collection's size and capacity are both equal to 5.
+    ASSERT_TRUE(collection->size(), 5);
+    ASSERT_TRUE(collection->capacity(), 5);
+}
+
+TEST_F(CollectionTest, AssignEntries_SetupCollection_ThrowAfterAccessIndex)
+{
+	// First, Add entries to populate collection
+    add_entries(2);
+
+	// Call setup to reset the collection
+    SetUp();
+
+	// Try to access entry that now will throw an out_of_range exception
+    ASSERT_THROW(collection->at(1), std::out_of_range);;
+}
